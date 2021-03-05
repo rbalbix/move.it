c7d066266d73ae1727631ecef875198a3d00fc7d
Ricardo Balbi <rbalbi@gmail.com>
2021-03-05 17:18:37 -0300

    feat: New Challenges

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
+import Head from 'next/head';
+import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
-import Head from 'next/head';
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
@@ -24,7 +25,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
