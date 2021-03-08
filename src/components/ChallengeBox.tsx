c1abb8823faae5938f91873bd4163ac55944a6e6
Ricardo Balbi <rbalbi@gmail.com>
2021-03-08 15:05:28 -0300

    feat: Context, Notifications

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,9 +1,23 @@
 import { useContext, useState } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+  const { activeChallenge, resetChallenge, completeChallenge } = useContext(
+    ChallengesContext
+  );
+  const { resetCountdown } = useContext(CountdownContext);
+
+  function handleChallengeSucceded() {
+    completeChallenge();
+    resetCountdown();
+  }
+
+  function handleChallengeFailed() {
+    resetChallenge();
+    resetCountdown();
+  }
 
   return (
     <div className={styles.challengeBoxContainer}>
@@ -24,11 +38,15 @@ export function ChallengeBox() {
             <button
               type='button'
               className={styles.challengeFailedButton}
-              onClick={resetChallenge}
+              onClick={handleChallengeFailed}
             >
               Falhei
             </button>
-            <button type='button' className={styles.challengeSuccededButton}>
+            <button
+              type='button'
+              className={styles.challengeSuccededButton}
+              onClick={handleChallengeSucceded}
+            >
               Completei
             </button>
           </footer>
